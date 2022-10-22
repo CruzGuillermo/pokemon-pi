8e00b24557651ea06cf841c669a3c52d6a9b61a2
cruzguillermo <guillermobelizcruz98@gmail.com>
2022-10-22 14:01:02 -0300

    index

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { store } from './Redux/store/index';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import axios from 'axios';
 import dotenv from 'dotenv'
 dotenv.config();
 
